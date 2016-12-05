1ef0c56f6d1b2d907dbefebfbeb3fb636b9be596
saudakhtar <saudakhtars@cybage.com>
2016-12-05 20:45:33 +0530

    corrected url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,7 +12,7 @@ export const environment = {
     // logout:'http://172.27.140.186:3000/api/sign_out'
     // ideas: 'api/ideas/idea.json'
     ideas:'https://ideatorpoc-backend.herokuapp.com/api/ideas/',
-    ideadAdd:'https://ideatorpoc-backend.herokuapp.com/api/ideas/create',
+    ideadAdd:'https://ideatorpoc-backend.herokuapp.com/api/ideas/',
     login:'https://ideatorpoc-backend.herokuapp.com/api/sign_in',
     logout:'https://ideatorpoc-backend.herokuapp.com/api/sign_out'
   }
