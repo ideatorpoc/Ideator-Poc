ba3d4d8481864196558e4b94097c80488d2d0d0e
ideatorpoc <ideatorpoc@gmail.com>
2016-12-22 11:29:30 +0530

    Adding tests for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,22 +1,41 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async,ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import {NO_ERRORS_SCHEMA} from '@angular/core';
 
 describe('App: IdeatorPoc', () => {
-  beforeEach(() => {
+  let component:    AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
       schemas:[NO_ERRORS_SCHEMA]
-    });
-  });
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+   });
 
   it('should create the app', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+     expect(component).toBeTruthy();
+  }));
+
+  it('should should have default theme', async(()=>{
+      expect(component.theme).toEqual('deeppurple-amber');
+  }));
+
+  it('it should have a different theme',async(()=>{
+     component.theme='indigo-pink';
+    fixture.detectChanges();
+    expect(component.theme).toEqual('indigo-pink');
   }));
 });
+
+
