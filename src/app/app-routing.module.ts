67763750524119298d832ba1c52dff383a5dee72
Vipin Mantri <vipinm@cybage.com>
2017-12-11 11:57:45 +0530

    comment added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import {Routes,RouterModule} from '@angular/router';
 
 const routes:Routes = [
     {path:'',pathMatch:'full',redirectTo:'ideas'}
-    //ToDo Add others here 
+    //ToDo Add others here abc
 ];
 
 
